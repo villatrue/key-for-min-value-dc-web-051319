7fb7d04ec11a9b5b8e3ad4ea1188e5964123cbc3
Sean Carter <scarter0313@gmail.com>
2019-05-11 02:44:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(hash)
   hash.each do |k, v|
     # binding.pry
     if lowest_value == nil || v < lowest_value
-      lowest_value = v 1
-      lowest_key = k adam
+      lowest_value = v  #1
+      lowest_key = k    #adam
     end
   end
   lowest_key
