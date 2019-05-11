894428022cf984a7a013acbb18d23c0d887c0397
Sean Carter <scarter0313@gmail.com>
2019-05-11 02:41:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,10 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
-require "pry"
+# require "pry"
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |k, v|
-    binding.pry
+    # binding.pry
     if lowest_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
